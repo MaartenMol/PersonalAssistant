eea7e1f34f29afc2d753bc14f13934f012eb20b1
Maarten Mol <maartenmol96@gmail.com>
2019-06-16 09:34:29 +0200

    update

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-# NOTE: this example requires PyAudio because it uses the Microphone class
+# Required for MIC: PyAudio
+
+speechLang = "nl-NL"
 
 import speech_recognition as sr
-
-import re
-import webbrowser
-import requests
-import xmltodict
+import re, webbrowser, requests, xmltodict, datetime
+from googlesearch import search
+from pyowm import OWM
 
 import win32com.client as wincl
 speak = wincl.Dispatch("SAPI.SpVoice")
@@ -30,7 +30,7 @@ def listenMic():
         # for testing purposes, we're just using the default API key
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
-        result = r.recognize_google(audio, language = "nl-NL")
+        result = r.recognize_google(audio, language = speechLang)
         print("Google Speech Recognition thinks you said: " + result)
         lookForAgent(result)
     except sr.UnknownValueError:
@@ -43,6 +43,8 @@ def lookForAgent(result):
         agent_NS(result)
     if "hey Philips".lower() in result.lower():
         agent_Philips(result)
+    if "hey Jarvis".lower() in result.lower():
+        agent_Jarvis(result)
 
 def agent_NS(result):
     if "welke treinen vertrekken" in result.lower():
@@ -109,6 +111,44 @@ def agent_Philips(result):
         
         bridge.set_light(room,'on', True)
         print("De lichten in de " + room + " staan nu aan!")
+
+def agent_Jarvis(result):
+    if "open reddit" in result.lower():
+        reg_ex = re.search('open reddit (.*)', result)
+        url = 'https://www.reddit.com/'
+        if reg_ex:
+            subreddit = reg_ex.group(1)
+            url = url + 'r/' + subreddit
+        webbrowser.open(url)
+        speak.Speak("Opening Reddit for you!")
+
+    if "zoek op" in result.lower():
+        reg_ex = re.search('zoek op (.*)', result)
+        url = 'https://www.google.com'
+        if reg_ex:
+            search = reg_ex.group(1)
+            url = url + '/search?q=' + search
+        webbrowser.open(url)
+        speak.Speak("Opening Google for you!")
+
+    if "hoe is het weer" in result.lower():
+        reg_ex = re.search('hoe is het weer in (.*)', result)
+        if reg_ex:
+            city= reg_ex.group(1)
+        owm = OWM(API_key='ab0d5e80e8dafb2cb81fa9e82431c1fa')
+        obs = owm.weather_at_place(city)
+        w = obs.get_weather()
+        k = w.get_status()
+        x = w.get_temperature(unit='celsius')
+        result = 'Current weather in %s is %s. The maximum temperature is %0.2f and the minimum temperature is %0.2f degree celcius' % (city, k, x['temp_max'], x['temp_min'])
+        print(result)
+        speak.Speak(result)
+
+    if "hoe laat" in result.lower():
+        now = datetime.datetime.now()
+        time = 'Current time is %d hours %d minutes' % (now.hour, now.minute)
+        print(time)
+        speak.Speak(time)
 
 #Define main APP
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,20 +1,5 @@
-# importing the requests library 
-import requests 
-  
-# api-endpoint 
-URL = "https://www.ns.nl/rio-reisinfo-api/service/stations?q=" + "Oss"
-  
-# sending get request and saving the response as response object 
-r = requests.get(url = URL) 
-  
-# extracting data in json format 
-data = r.json() 
-  
-  
-# extracting latitude, longitude and formatted address  
-# of the first matching location 
-station_id = data[0]['id'] 
-station_naam = data[0]['naam'] 
-  
-# printing the output 
-print(station_id + " " + station_naam) 
+import pyttsx3
+engine = pyttsx3.init()
+
+engine.say("The Reddit content has been opened for you Sir.")
+engine.runAndWait()
